2ffff2fecc4d71477a91bcfbf97b76b44d1fcc52
Craig Shih <cshih1141@gmail.com>
2020-01-18 10:47:04 -0800

    initial server query

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import Event from './Event';
+const axios = require('axios');
 
-const App = () => {
-  return (
-    <div>
-      THIS IS AN HISTORICAL EVENTS FINDER
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      events: [],
+    };
+
+    this.retrieveHistEvents = this.retrieveHistEvents.bind(this);
+  }
+
+  retrieveHistEvents(query) {
+    axios.get('/events?_page=1&_limit=10&q=' + query)
+      .then((response) => {
+        // handle success
+        // console.log(response); 
+        this.setState({
+          events: response.data
+        }, () => console.log(this.state.events));
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+  }
+
+  componentDidMount() {
+    this.retrieveHistEvents('hello');
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.events.map((event) => <Event histEvent = {event}/>)}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/Event.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Event = ({histEvent}) => (
+  <div>
+    {histEvent.description}
+  </div>
+);
+
+export default Event;
